77c707cafe2c0f7d87d9716bc39bb3e40bd37edf
penguinspizza <keitousers@gmail.com>
2020-10-27 05:49:57 +0000

    set MySQL

--- outputproject2/settings.py
+++ outputproject2/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'outputproject2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'penguinspizza$outputproject2',
+        'USER': 'penguinspizza',
+        'PASSWORD': 'Rpc01md35',
+        'HOST': 'penguinspizza.mysql.pythonanywhere-services.com',
     }
 }
 
